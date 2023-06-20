788026e2ba9d60619b86278a69994d69dadf832c
JoseAdrianRodriguezGonzalez <88957069+JoseAdrianRodriguezGonzalez@users.noreply.github.com>
2023-06-20 10:08:48 -0600

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
